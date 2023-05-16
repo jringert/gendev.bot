56dbaa3f3b13745dfd0aae02cf429e050ae49f71
Jan Oliver Ringert <jan.ringert@uni-weimar.de>
2023-05-16 15:34:13 +0200

    added access to GUI instance

--- gendev.bot.runner/src/gendev/bot/runner/RobotRunnerGui.java
+++ gendev.bot.runner/src/gendev/bot/runner/RobotRunnerGui.java
@@ -1,5 +1,6 @@
 package gendev.bot.runner;
 
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -97,6 +98,10 @@ public class RobotRunnerGui extends JPanel implements ActionListener {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	public static RobotRunnerGui instance() {
+		return instance;
 	}
 
 }
