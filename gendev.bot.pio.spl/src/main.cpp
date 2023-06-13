8a57393b544f57742e9de7b784e48997e8e05435
Jan Oliver Ringert <jan.ringert@uni-weimar.de>
2023-06-14 00:29:26 +0200

    ifdef spl created

--- gendev.bot.pio.spl/src/main.cpp
+++ gendev.bot.pio.spl/src/main.cpp
@@ -1,52 +1,59 @@
-// --- INCLUDES -----------------------------------------------------------------------------------
+// --- CONFIG -------------------------------------------------------------------------------------
+//#define STARTUP_DELAY // wait 5 seconds at every startup to connect to serial monitor
+#define USE_LAPTOP // use hotspot on laptop as access point
+//#define USE_ROUTER // use router as access point
+#define TRACE // print debug messages to serial monitor
+//#define USE_MOTORS // use motors
+#define USE_USS // use ultrasonic sensor
+//#define USE_TOUCH // use touch sensor as emergency stop
+//#define USE_WIFI // use WiFi to connect to server
 
+// --- INCLUDES -----------------------------------------------------------------------------------
 #include <Arduino.h>
+#ifdef USE_WIFI
 #include <WiFi.h>
 #include "TCPRequest.h"
+#endif
+
 
 // --- DEFINES ------------------------------------------------------------------------------------
+// See pin problems https://randomnerdtutorials.com/esp8266-pinout-reference-gpios/
 
-#define USE_LAPTOP
-
+#ifdef USE_MOTORS
 #define L298N_IN3 26 // A-1A
 #define L298N_IN4 27 // A1-B
 #define L298N_IN2 25 // B-1A
 #define L298N_IN1 33 // B-1B
+#endif
 
-// direction of motors -1 BWD, 0 STP, 1 FWD
-int dirA = 0;
-int dirB = 0;
-
-// time in ms to wait between direction changes
-const long waitDirChange = 100;
-
-// See pin problems https://randomnerdtutorials.com/esp8266-pinout-reference-gpios/
+#ifdef USE_USS
 #define TRIGGER_PIN 5
 #define ECHO_PIN 18
+#endif
 #define MAX_DISTANCE 100 // Maximum distance
 
-#define TOUCH_PIN 
+#ifdef USE_TOUCH
+#define TOUCH_PIN 32
+#endif
 
-#define STARTUP_DELAY 0 // always wait $ ms before going into the loop
+#define DBG_DELAY 0   // wait $ ms longer for timeouts to debug
 
+#ifdef USE_WIFI
 #ifdef USE_LAPTOP
 String WIFI_NAME = "jor-lap";
 String WIFI_PWD = "********";
 String SERVER_IP = "192.168.137.1";
 uint16_t SERVER_PORT = 5000;
-#else
+#endif
+#ifdef USE_ROUTER
 String WIFI_NAME = "Testing";
 String WIFI_PWD = "SE_2022_WLAN";
 String SERVER_IP = "192.168.1.247";
 uint16_t SERVER_PORT = 5000;
 #endif
 
-unsigned long previousMillis = 0;
-
 uint16_t TCP_TIMEOUT = 500;
 TCPRequest tcp_conn(SERVER_IP, SERVER_PORT);
-
-#define DBG_DELAY 1000
 
 // --- FUNCTIONS ----------------------------------------------------------------------------------
 
@@ -55,18 +62,26 @@ void wifi_setup(String wifi_name, String wifi_pwd)
   WiFi.mode(WIFI_MODE_STA); // set WiFi to station == client
   WiFi.begin(wifi_name.c_str(), wifi_pwd.c_str());
 
-  Serial.print("Connecting WiFi ");
+  #ifdef TRACE
+  Serial.print("Connecting WiFi "); 
+  #endif
 
   while (WiFi.status() != WL_CONNECTED)
   {
+    #ifdef TRACE
     Serial.print(".");
+    #endif
     delay(250);
   }
 
+  #ifdef TRACE
   Serial.print(" done. My IP is ");
   Serial.println(WiFi.localIP().toString());
+  #endif
 }
+#endif
 
+#ifdef USE_MOTORS
 void forwardA()
 {
   digitalWrite(L298N_IN1, HIGH); // direction = forward
@@ -102,9 +117,11 @@ void backB()
   digitalWrite(L298N_IN3, LOW);  // direction = forward
   digitalWrite(L298N_IN4, HIGH); // PWM speed = slow
 }
+#endif
 
 int getDistance()
 {
+  #ifdef USE_USS
   // pulse the USS
   digitalWrite(TRIGGER_PIN, LOW);
   delayMicroseconds(2);
@@ -122,29 +139,40 @@ int getDistance()
   }
   else
   {
+  #endif
     return MAX_DISTANCE;
+  #ifdef USE_USS
   }
+  #endif
 }
 
-// --- SETUP --------------------------------------------------------------------------------------
 
 void setup()
 {
-  delay(STARTUP_DELAY);
+  #ifdef STARTUP_DELAY
+  delay(5000); // wait 5 seconds for serial monitor
+  #endif
 
+  #ifdef TRACE
   Serial.begin(115200);
+  #endif
 
+  #ifdef USE_WIFI
   wifi_setup(WIFI_NAME, WIFI_PWD);
 
   tcp_conn.setTimeout(TCP_TIMEOUT);
-  // tcp_conn.setTimeout(DBG_DELAY + TCP_TIMEOUT);
+  tcp_conn.setTimeout(DBG_DELAY + TCP_TIMEOUT);
   tcp_conn.setReuseConnection(true);
+  #endif
 
-  // USS
+  #ifdef USE_USS  
+  // set up USS pins
   pinMode(TRIGGER_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
+  #endif
 
-  // motor pins
+  #ifdef USE_MOTORS
+  // set up motor pins
   pinMode(L298N_IN1, OUTPUT);
   pinMode(L298N_IN2, OUTPUT);
   digitalWrite(L298N_IN1, LOW);
@@ -154,33 +182,41 @@ void setup()
   pinMode(L298N_IN4, OUTPUT);
   digitalWrite(L298N_IN3, LOW);
   digitalWrite(L298N_IN4, LOW);
+  #endif
 }
-
-// --- MAIN ---------------------------------------------------------------------------------------
 
 void loop()
 {
-  // ---- MAIN SECTION ------------------------------------
-
-  if (touchRead(32) < 20)
+  #ifdef USE_TOUCH
+  if (touchRead(TOUCH_PIN) < 20)
   {
+    #ifdef USE_MOTORS
     stopA();
     stopB();
+    #endif
     delay(100);
     return;
   }
+  #endif
+
   int distance = getDistance();
 
-  /* TODO remove */ Serial.printf("DBG::distance = %d\n", distance); // TODO remove
+  #ifdef TRACE
+  Serial.printf("DBG::distance = %d\n", distance);
+  #endif
 
+  #ifdef USE_WIFI
   tcp_conn.setPayload(String(distance));
   tcp_conn.run();
 
   if (!tcp_conn.isTimedOut())
   {
     String response = tcp_conn.getResponse();
+    #ifdef TRACE
     Serial.println(response);
+    #endif
 
+    #ifdef USE_MOTORS
     char leftM = response.charAt(0);
     if (leftM == 'F')
     {
@@ -208,11 +244,18 @@ void loop()
     {
       stopA();
     }
+    #endif
   }
   else
   {
-    Serial.printf("DBG::tcp timeout\n"); // TODO remove
+    #ifdef TRACE
+    Serial.printf("DBG::tcp timeout\n");
+    #endif
+
+    #ifdef USE_MOTORS
     stopA();
     stopB();
+    #endif
   }
+  #endif
 }
